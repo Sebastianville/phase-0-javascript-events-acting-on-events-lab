ff466f2cdbeafbfae8fd2932ac55ccc0cddccff5
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-06-23 16:37:49 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+const moveLeft = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = moveLeft.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+        moveLeft.style.left = `${left - 1}px`;
+    }
+  }
+
+  const moveRight = document.getElementById("dodger");
+function moveDodgerRight() {
+    const rightNumbers = moveRight.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+  
+    if (left > 0) {
+        moveRight.style.left = `${left + 1}px`;
+    }
+  }
